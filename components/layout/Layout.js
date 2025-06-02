2c3a2fc5725ac1d4537f32e87795db10bb2fed9e
Alireza Jahangiri <al.jahangiri82@gmail.com>
2025-06-03 01:36:13 +0330

    finalize footer styles for desktop and mobile

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -21,7 +21,36 @@ function Layout({ children }) {
       <div className={styles.container}>{children}</div>
 
       {/* FOOTER */}
-      <footer>khodafex</footer>
+      <footer className={styles.footer}>
+        <div className={styles.socials}>
+          <Image src="/images/carvago-text.svg" width="100" height="70"></Image>
+          <p>Carvago 2025 * All Rights Reserved &copy;</p>
+          <div className={styles.socialLinks}>
+            <Image src="/images/youtube.svg" width="25" height="25"></Image>
+            <Image src="/images/linkedin.svg" width="25" height="25"></Image>
+            <Image src="/images/facebook.svg" width="25" height="25"></Image>
+            <Image src="/images/telegram.svg" width="25" height="25"></Image>
+          </div>
+        </div>
+        <div className={styles.helpfulTags}>
+          <div className={styles.carvago}>
+            <h3>Carvago</h3>
+            <p>Buy</p>
+            <p>Reviews</p>
+            <p>How does it work?</p>
+            <p>Available</p>
+            <p>Hybrid</p>
+          </div>
+          <div className={styles.services}>
+            <h3>Services</h3>
+            <p>CarAudit</p>
+            <p>Delivery</p>
+            <p>Financing</p>
+            <p>gurantee</p>
+          </div>
+        </div>
+      </footer>
+      <div className={styles.creator}>Made with ❤️ by Alireza Jahangiri</div>
     </>
   );
 }
