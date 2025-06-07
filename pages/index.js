5818e66b376fbcc5263083636b939a619a5f3e89
Alireza Jahangiri <al.jahangiri82@gmail.com>
2025-06-07 21:49:30 +0330

    working on home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-export default function Home() {
+import SearchBar from "@/components/module/SearchBar";
+import Categories from "@/components/module/Categories";
+import CarsPage from "@/components/templates/CarsPage";
+import carsData from "@/data/carsData";
+
+function Index() {
+  const cars = carsData.slice(0, 3);
   return (
     <div>
-      <h1>Carvago</h1>
+      <SearchBar />
+      <Categories />
+      <CarsPage data={cars} />
     </div>
   );
 }
+
+export default Index;
